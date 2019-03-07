3cd26aae7a2110db9cc2aec6d0ad42ff9a9c5398
SanaChinaliyeva <sana.chinaliyeva@gmail.com>
2019-03-07 18:23:18 +0600

    Вывожу названия границ

--- src/components/FullCountry/FullCountry.js
+++ src/components/FullCountry/FullCountry.js
@@ -1,22 +1,45 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import uuid from 'uuid';
 import './FullCountry.css';
 
 class FullCountry extends Component {
     state ={
-        loadedCountry: null
+        loadedCountry: null,
+        borderNames: null,
+        borderCodes: null
     };
     isNeededToGetCountry = () => {
         return this.props.name && (!this.state.loadedCountry || (this.state.loadedCountry.name !== this.props.name));
     };
-    componentDidUpdate () {
-        const BASE_URL = 'https://restcountries.eu/rest/v2/name/';
+    getCountryByName = () => {
+        const BASE_URL = 'https://restcountries.eu/rest/v2/';
         if (this.isNeededToGetCountry()) {
-            axios.get (BASE_URL + this.props.name)
-                .then (response => {
-                    this.setState ({loadedCountry: response.data[0]});
-                });
+            axios.get (BASE_URL + 'name/' + this.props.name).then (response => {
+                const country = response.data[0];
+                const borderCodes = country.borders;
+                const borderNames = [];
+                if (borderCodes) {
+                    borderCodes.map (code => {
+                        axios.get (BASE_URL + 'alpha/' + code).then (response => {
+                            const border = response.data.name;
+                            borderNames.push (border);
+                            return borderNames;
+                        }).then (borderNames => {
+                            this.setState ({borderCodes: country.borders, borderNames});
+                        }).catch (error => {
+                            console.log (error);
+                        });
+                    });
+                }
+                this.setState ({loadedCountry: country, borderCodes: null, borderNames: null});
+            }).catch(error => {
+                console.log(error);
+            });
         }
+    };
+    componentDidUpdate () {
+        this.getCountryByName();
     }
     render () {
         if (this.state.loadedCountry) {
@@ -27,6 +50,14 @@ class FullCountry extends Component {
                         <p className="FullCountry_Desc"><b>Capital: </b>{this.state.loadedCountry.capital}</p>
                         <p className="FullCountry_Desc"><b>Population: </b> {this.state.loadedCountry.population}</p>
                         <p className="FullCountry_Desc"><b>Alternative: </b> {this.state.loadedCountry.altSpellings.join(", ")}</p>
+                        <p className="FullCountry_Desc"><b>Borders: </b></p>
+                        <ul>
+                            {
+                                this.state.borderNames ?
+                                    this.state.borderNames.map (border => <li key={uuid ()}>{border}</li>)
+                                    : <p>Borders are unknown</p>
+                            }
+                        </ul>
                     </div>
                     <img src={this.state.loadedCountry.flag} alt="Flag" />
                 </div>
--- src/containers/App.js
+++ src/containers/App.js
@@ -10,41 +10,41 @@ class App extends Component {
         countries: [],
         selectedCountry: null
     }
-  getCountriesList = () => {
-      let countries = [...this.state.countries];
-    axios.get().then(response => {
-      return Promise.all(response.data.map(country => {
-        countries.push(country.name);
-          this.setState({countries});
-          return country;
-      }));
-    }).catch(error => {
-      console.log(error);
-    });
-  };
-  componentDidMount () {
-    this.getCountriesList();
-  }
+    getCountriesList = () => {
+        let countries = [...this.state.countries];
+        axios.get().then(response => {
+            return Promise.all(response.data.map(country => {
+                countries.push(country.name);
+                this.setState({countries});
+                return country;
+            }));
+        }).catch(error => {
+            console.log(error);
+        });
+    };
+    componentDidMount () {
+        this.getCountriesList();
+    }
 
-  handleClick = (country) => {
-      console.log(country);
-      this.setState({selectedCountry: country})
-  };
+    handleClick = (country) => {
+        console.log(country);
+        this.setState({selectedCountry: country})
+    };
 
-  render() {
-    return (
-      <div className="App">
-            <div className="choicePanel">
-                {
-                    (this.state.countries).map (country => {
-                        return <h2 onClick={() => this.handleClick(country)} className="countryName" key={uuid()}>{country}</h2>
-                    })
-                }
+    render() {
+        return (
+            <div className="App">
+                <div className="choicePanel">
+                    {
+                        (this.state.countries).map (country => {
+                            return <h2 onClick={() => this.handleClick(country)} className="countryName" key={uuid()}>{country}</h2>
+                        })
+                    }
+                </div>
+                <FullCountry name={this.state.selectedCountry} />
             </div>
-          <FullCountry name={this.state.selectedCountry} />
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
